1e7314fc6bacb4f1a3ae36e795a354670ae0ad47
Prema <lapremavera@gmail.com>
2017-09-21 20:21:33 +0200

    groentenlijst met grid bijgevoegd

--- src/main/resources/public/js/index.js
+++ src/main/resources/public/js/index.js
@@ -5,3 +5,29 @@ $('.toggle').on('click', function() {
 $('.close').on('click', function() {
   $('.container').stop().removeClass('active');
 });
+
+ $("#groenten-zoek-form").submit(function(e) {
+                e.preventDefault();
+
+                let postcode = $(this.postcode).val();
+                document.write(postcode);
+
+                loadGroentenLijstByPostcode(postcode);
+
+                });
+
+                function loadGroentenLijstByPostcode(postcode) {
+
+                    $.getJSON('/api/plantensoort/byPostcode/'.concat(postcode), function (plantensoorten) {
+
+                    document.writeln();
+                    for (let plantensoort of plantensoorten)
+                    {
+                        document.write(plantensoort.naamSoort);
+                        document.writeln();
+                    }
+                });
+
+                }
+
+
