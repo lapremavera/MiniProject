ae854b45c33142528e6be99b0aece01b06fef85c
Prema <lapremavera@gmail.com>
2017-09-25 20:59:10 +0200

    commit maandag voor submit

--- src/main/resources/public/js/voegJeGroenteToe.js
+++ src/main/resources/public/js/voegJeGroenteToe.js
@@ -1,3 +1,59 @@
-$("groenteInvulformulier").submit(function(e)) {
+$("groenteInvulformulier").submit(function(e) {
     e.preventDefault();
-}
+
+
+    document.writeline("Gelukt");
+
+                let groente = {
+                    naamSoort: $(this.plantennaam).val(),
+                    naamLatijn: $(this.latijnse_naam).val(),
+                    groeiVoorwaarden: $(this.groeiVoorwaarden).val(),
+                    beschrijvingPlantsoort: $(this.comment).val()
+
+                };
+
+                $.ajax({
+                    method: 'post',
+                    url: '/api/plantensoort',
+                    contentType: 'application/json',
+                    data: JSON.stringify(groente),
+                    processData: false,
+                    success: function() {
+                        // Refresh data table
+                      //  loadGroentenlijst();
+
+                    }
+                });
+
+                this.reset();
+            });
+
+           // loadGroentenlijst();
+
+
+            function loadGroentenlijst() {
+
+             $.getJSON('/api/plantensoorten/all', function (plantensoort) {
+                    let $tbody = $("#groentenlijst-data").empty();
+
+                    for (let groente of plantensoorten) {
+                        let $removeButton = $("<button>").addClass("btn").addClass("btn-danger").text("Remove");
+                        $removeButton.click(function(e) {
+                            $.ajax({
+                                method: 'delete',
+                                url: `/api/plantensoort/${plantensoort.id}`,
+                                success: loadGroentenlijst
+                            });
+                        });
+
+                        let $row = $('<tr>')
+                            .append($('<td>').text(plantensoort.id))
+                            .append($('<td>').text(plantensoort.plantennaam))
+                            .append($('<td>').text(plantensoort.latijnseNaam))
+                            .append($('<td>').text(plantensoort.groeivoorwaarden))
+                            .append($('<td>').text(plantensoort.info))
+                            .append($('<td>').append($removeButton));
+                        $tbody.append($row);
+                    }
+                });
+            }
